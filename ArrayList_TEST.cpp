26c99ebb70edc6fcb26f45df75ba5fe44e2d7cd9
rsilva <rsilva@cs-122>
2019-10-27 22:13:44 -0400

    pushing to test ArrayList

--- ArrayList_TEST.cpp
+++ ArrayList_TEST.cpp
@@ -41,26 +41,47 @@ TEST_CASE("Testing ArrayList template"){
         a.pushBack(7);
         a.pushBack(84);
         REQUIRE(a.getBack() == 84);
+        REQUIRE(a.getSize() == 3);
 
         a.popBack();
         REQUIRE(a.getBack() == 7);
         REQUIRE(a.getCapacity() == 10);
+        REQUIRE(a.getSize() == 2);
+        REQUIRE(a.isEmpty() == false);
+        a.popBack();
+        a.popBack();
+        REQUIRE(a.isEmpty() == true);
+        REQUIRE(a.getSize() == 0);
     }
 
-    SECTION("getBack"){
+    SECTION("getItem & setItem"){
+        ArrayList<double> a;
+        a.pushFront(56);
+        a.pushBack(45);
+        a.pushFront(8);
 
+        REQUIRE(a.getItem(0) == 8);
+        REQUIRE(a.getItem(1) == 56);
+        REQUIRE(a.getItem(2) == 45);
+
+        a.setItem(2, 489);
+        a.setItem(1, 5);
+        REQUIRE(a.getItem(1) == 5);
+        REQUIRE(a.getItem(2) == 489);
     }
 
-    SECTION("pushFront"){
-
-    }
-
-    SECTION("popFront"){
-        
-    }
-
-    SECTION("getFront"){
-        
+    SECTION("remove"){
+        ArrayList<int> a;
+        a.pushFront(56);
+        a.pushBack(45);
+        a.pushFront(8);
+        a.pushFront(56);
+        a.pushBack(23);
+        a.pushBack(487);
+        REQUIRE(a.getSize() == 6);
+        a.remove(4);
+        REQUIRE(a.getSize() == 5);
+        REQUIRE(a.getItem(4) == 487);
     }
 
 }
