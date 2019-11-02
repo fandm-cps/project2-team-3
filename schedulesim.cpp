8518f954cd310953861d4f7c61eac6fdcd992e26
jkupa <jkupa@cs-122>
2019-11-02 19:51:06 -0400

    added the updated schedulesim

--- schedulesim.cpp
+++ schedulesim.cpp
@@ -17,15 +17,41 @@ int main(int argc, char* argv[])
 
     ArrayList<Scheduler*> arr;
     arr.pushBack(&myRob);
-    arr.pushBack(&fastRob);
+    //arr.pushBack(&fastRob);
     string strArr [2] = {"RoundRobin", "FastRR"};
 
-    cout << "Results: " << endl;
+    //cout << "Results: " << endl;
     double* results;
-    for(int i = 0; i < 2; i++){
-        results = simulate(arr.getItem(i), numCPU, numIO, numCycles);
-        cout << strArr[i] << ": " << results << endl; 
-    }
+    
+    results = simulate(&myRob, numCPU, numIO, numCycles);
+    cout << "REG RR Overhead: " << results[0] << endl;
+    cout << "REG RR CPUtime (CPUBound): " << results[1] << endl;
+    cout << "REG RR WAITtime (CPUBound): " << results[2] << endl;
+    cout << "REG RR CPUtime (IOBound): " << results[3] << endl;
+    cout << "REG RR WAITtime (IOBound): " << results[4] << endl;
+    /**for(int i = 0; i < 5; i++){
+      cout << "i = " << i << endl;
+        
+      //cout << "lets find out what index 1 is: " << results[1] << endl;
+        cout << "Round Robin: " << results[i] << endl; 
+	}**/
+
+    double* fastResults;
+
+    fastResults = simulate(&fastRob, numCPU, numIO, numCycles);
+
+    //for(int i = 0; i < 5; i++){
+    //cout << "i = " << i << endl;
+        
+      //cout << "lets find out what index 1 is: " << results[1] << endl;
+        cout << "FAST RR Overhead: " << fastResults[0] << endl;
+	cout << "FAST RR CPUtime (CPUBound): " << fastResults[1] << endl;
+	cout << "FAST RR WAITtime (CPUBound): " << fastResults[2] << endl;
+	cout << "FAST RR CPUtime (IOBound): " << fastResults[3] << endl;
+	cout << "FAST RR WAITtime (IOBound): " << fastResults[4] << endl;
+	
+	//}
+    
 
     //Simulate newSim;
     //double* regResults = simulate(&myRob, numCPU, numIO, numCycles);
